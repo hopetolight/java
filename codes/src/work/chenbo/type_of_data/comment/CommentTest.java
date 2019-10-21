2ce453b6c3971201f9de67b130d1a5c60d728be7
ChenBo <hopetolight@outlook.com>
2019-10-21 23:48:09 +0800

    [add] 单行注释和多行注释

---
+++ codes/src/work/chenbo/type_of_data/comment/CommentTest.java
@@ -0,0 +1,17 @@
+package work.chenbo.type_of_data.comment;
+
+/**
+ * @className CommentTest
+ * @authtor ChenBo
+ * @date 2019/10/21
+ */
+public class CommentTest {
+    /*
+    * 这是多行注释
+    */
+    public static void main(String[] args){
+        //这是单行注释
+        System.out.println("Hello Word!");
+        //System.out.println("这行被注释了,将不会执行");
+    }
+}
